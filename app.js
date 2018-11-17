ccdb4b4ac8e8cb829c9598855b829bc4feb9c932
Test Testsson <olsson.bjoern@gmail.com>
2018-11-17 06:52:21 +0100

    Första commiten

---
+++ app.js
@@ -0,0 +1,111 @@
+/* IMPORT */
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var jsonfile = require("jsonfile");
+
+/* READ JSONFILE */
+
+var file = "courses.json";
+var course = [];
+
+jsonfile.readFile(file, function(err, obj) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(obj),
+        courses = obj;
+    }
+})
+
+/* MAKE INSTANCE OF EXPRESS */
+var app = express();
+
+// EXAMPLE OF MIDDLEWARE, something you can do inside of loop and then go on to the next thing
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false } ));
+
+//STATIC ROUTE
+app.use(express.static(path.join(__dirname, 'public')));
+
+//REST-API for courses (same as routing)
+
+//Send all courses
+app.get("/courses", function(req, res) {
+    console.log("Send all courses");
+    res.send(courses);
+});
+
+
+
+// Add course
+app.post("/courses/add", function(req, res) {
+    //Get next id
+    var newId = getNextId(courses);
+
+    //Create new object
+    var newCourse = {
+        _id: newId,
+        courseId: req.body.courseId,
+        courseName: req.body.courseName,
+        coursePeriod: req.body.coursePeriod        
+    }
+
+    //Add to the array
+    courses.push(newCourse);
+
+    //Save to file
+    saveFile();
+
+    res.redirect("/");
+
+
+    //res.send({"message": "Adding course"});
+});
+
+
+
+// Delete course
+app.delete("/courses/course/delete/:id", function(req, res) {
+    var deleteId = req.params.id;
+
+    //Find right course to delete
+    for(var i=0; i<courses.length; i++) {
+        if(courses[i].id == deleteId) {
+            courses.splice(i, 1);
+        }
+    }
+
+    //Save to file
+    saveFile();
+
+    res.send({ "message" : "Deleting course with ID " + deleteId});
+});
+
+//SAVE JSON-file
+function saveFile() {
+    jsonfile.writeFile(file, courses, function(err) {
+        console.log(err);
+    });
+}
+
+// Get highest id
+function getNextId(arr) {
+    var max = 0;
+
+    for(var i=0; i<arr.length; i++) {
+        var current = parseInt(arr[i].id);
+        if(current > max) { max = current; }
+    }
+
+    return max + 1;
+}
+
+// PORT OF CONNECTION
+var port = 3000;
+
+// START SERVER
+app.listen(port, function() {
+    console.log("Server up and running on port " + port);
+});
---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+window.onload = loadCourses();
+
+
+//Read courses with AJAX
+function loadCourses() {
+    $.getJSON("http://localhost:3000/courses", function(data) {
+        
+        //Clear list
+        //$("#courselist").html("");
+        for(var i=0; i<data.length; i++) {
+            $("#courselist").append(
+                "<li>" + data[i].courseId + " " + data[i].courseName + " " + data[i].coursePeriod + " <button onclick='deleteCourse("+ data[i]._id + ")'>Delete</button> <button onclick='getSingleCourse("+ data[i]._id + ")'>Show course</button> </li>")
+        }
+
+    });
+}
+
+function getSingleCourse(id) {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/courses/course" + id 
+    }).done(function(response) {
+        console.log(response);
+    });
+}
+
+function deleteCourse(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:3000/courses/delete" + id 
+    }).done(function(response) {
+        console.log(response);
+    });
+}
